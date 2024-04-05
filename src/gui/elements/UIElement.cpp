397f80b94c50b903860df7389ff67e94536211fc
Ben Fields <fieldsben5@gmail.com>
2024-04-04 22:46:31 -0700

    Finished UIElement

--- src/gui/elements/UIElement.cpp
+++ src/gui/elements/UIElement.cpp
@@ -8,6 +8,27 @@ UIElement::UIElement() : selected(false)
 {
 }
 
+void UIElement::init(Adafruit_GFX*  _display,
+                     int16_t        _x,
+                     int16_t        _y,
+                     int16_t        _w,
+                     int16_t        _h,
+                     const uint8_t* _select_icon,
+                     uint16_t       _select_color,
+                     uint16_t       _default_color,
+                     uint16_t       _background_color)
+{
+    display          = _display;
+    x                = _x;
+    y                = _y;
+    w                = _w;
+    h                = _h;
+    select_icon      = _select_icon;
+    select_color     = _select_color;
+    default_color    = _default_color;
+    background_color = _background_color;
+}
+
 void UIElement::draw()
 {
     if (selected)
